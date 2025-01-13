2def978d63703428acf9348758bbe558c166c8fe
Bainomugisha Libra <148781031+BainoLibra@users.noreply.github.com>
2025-01-13 18:17:22 +0300

    Create object_methods.js

---
+++ object_methods.js
@@ -0,0 +1,21 @@
+// object.assign()
+let target ={a:1};
+let source ={b:2, c:3};
+Object.assign(target, source);
+console.log(target)
+// Object.getOwnPropertyDescriptor()
+let obj ={a:1};
+let descriptor =Object.getOwnPropertyDescriptor(obj, "a");
+console.log(descriptor);
+
+const obj1 ={b:2};
+const descriptors =Object.getOwnPropertyDescriptor(obj1, 'b');
+console.log(descriptors)
+
+// const obj2 ={b:2};
+// const descriptorss =Object.getOwnPropertyDescriptors(obj2, 'b');
+// console.log(descriptorss)
+
+// Object.getOwnPropertyDescriptors()
+const descript ={a:1, b:2, c:3}
+console.log(Object.getOwnPropertyDescriptors(descript))
